0468cca4e24756121bd5ae129b980b04b83b1ab8
Tanujkumar24 <tanuj.mangalapally@gmail.com>
2024-03-23 06:44:01 +0530

    commit second

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 #we use setup.py file , for installing the local package in my virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Tanujkumar',
+    author_email='tanuj.mangalapally@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
+#where ever you find __init__.py , it will consider it as a package
